6f21e6ed64f5da4db035ab3841d18863d7294289
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-08-31 20:31:17 +0300

    Regenerate for framework release v0.2.51

--- analyticsservice/make.generated.mk
+++ analyticsservice/make.generated.mk
@@ -21,10 +21,10 @@ endif
 
 ifneq ($(strip $(DEPENDENCY_PROXY_DIR)),)
 ifeq ($(origin RUSTSERVICELIB_SOURCE_CONTEXT),undefined)
-export RUSTSERVICELIB_SOURCE_CONTEXT := $(DEPENDENCY_GIT_MIRROR_DOCKER_BASE)/github.com/gorundebug/rustservicelib.git\#v0.2.50
+export RUSTSERVICELIB_SOURCE_CONTEXT := $(DEPENDENCY_GIT_MIRROR_DOCKER_BASE)/github.com/gorundebug/rustservicelib.git\#v0.2.51
 endif
 ifneq ($(strip $(USE_LOCAL_MODULES)),1)
-export EXAMPLE_MODEL_SOURCE_CONTEXT := $(DEPENDENCY_GIT_MIRROR_DOCKER_BASE)/github.com/gorundebug/rustexample.git\#v0.2.50
+export EXAMPLE_MODEL_SOURCE_CONTEXT := $(DEPENDENCY_GIT_MIRROR_DOCKER_BASE)/github.com/gorundebug/rustexample.git\#v0.2.51
 endif
 endif
 
